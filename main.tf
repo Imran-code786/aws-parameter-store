87d7510f013a4d5fdd13520085697d6972465c0f
Imran <imrankhanmohammed130@gmail.com>
2026-01-14 15:04:19 -0600

    kk

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_ssm_parameter" "params" {
+    count = length(var.params)
+    name = var.params[count.index].name
+    type = var.params[count.index].type
+    value = var.params[count.index].value
+}
+
+variable "params" {
+   default = [
+    {name = "demo", value = "demo1", type ="string"},
+    {name = "demo", value = "demo1", type ="string"}
+   ]
+}
